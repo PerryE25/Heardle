be4b71774a04e24d46487ed02efc1e3e80061a1c
PerryE25 <perryehimuh@gmail.com>
2026-07-01 16:43:35 -0500

    made play button circular

---
+++ Heardle/CustomButton.swift
@@ -0,0 +1,20 @@
+//
+//  CustomButton.swift
+//  Heardle
+//
+//  Created by Ehimuh, Perry on 7/1/26.
+//
+
+import UIKit
+
+class CustomButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
