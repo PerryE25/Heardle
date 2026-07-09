e323af49ff8b9790f834522553a7cb354b5f6f07
PerryE25 <perryehimuh@gmail.com>
2026-07-09 16:04:14 -0500

    added search feature of my current songs so far to choose from

---
+++ Heardle/SearchView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchView.swift
+//  Heardle
+//
+//  Created by Ehimuh, Perry on 7/9/26.
+//
+
+import UIKit
+
+class SearchView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
