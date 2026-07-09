e46f5ca6d675fb981883b90b66c05a5aad060907
PerryE25 <perryehimuh@gmail.com>
2026-07-09 18:57:58 -0500

    added a header i didnt have before

--- Heardle/SearchView.swift
+++ Heardle/SearchView.swift
@@ -3,6 +3,10 @@
 //  Heardle
 //
 //  Created by Ehimuh, Perry on 7/9/26.
+//  Project: Heardle
+//  Team Number: 3
+//  Team Members: Jeremiah Franklin, Victor Sanchez, Haroon Memon, Perry Ehimuh
+//  Course: CS371L
 //
 
 import UIKit
@@ -11,6 +15,7 @@ protocol SearchViewDelegate: AnyObject {
     func searchViewDidTapSearch(_ searchView: SearchView)
 }
 
+// A custom view where I can allow segues to full-screen search bar VCs
 class SearchView: UIView {
 
     weak var delegate: SearchViewDelegate?
