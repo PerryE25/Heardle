3e646cb2b84386005f3ec5e233f5f21314774196
Jeremiah <Jeremiahbafr122@gmail.com>
2026-07-24 01:25:14 -0500

    Added PreviousAttemptsVC

--- Heardle/PreviousAttemptsViewController.swift
+++ Heardle/PreviousAttemptsViewController.swift
@@ -11,15 +11,52 @@
 
 import UIKit
 
-class PreviousAttemptsViewController: UIViewController {
-
+class PreviousAttemptsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var exitButton: UIButton!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var prevGuessesTitle: UILabel!
+    // This will hold only the actual failed guesses, no nils.
+    var validGuesses: [Song] = []
+    
+    let cellID = "GuessCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        validGuesses = prevGuesses.compactMap { $0 } // Removes the nils from the guesses array
+        tableView.delegate = self
+        tableView.dataSource = self
+        self.view.overrideUserInterfaceStyle = .dark
+        prevGuessesTitle.font = UIFont.systemFont(ofSize: 20, weight: .bold)
     }
     
-
-    // prev guesses are either display the song or if the prevGuess[currentIndex] is nil, then that means I skipped the song and display wtv was on the demo video
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return validGuesses.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
+        let song = validGuesses[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        content.text = "\(song.name)"
+        content.secondaryText = "Song • \(song.artist)"
+        if let data = song.albumArtData {
+            content.image = UIImage(data: data)
+        } else {
+            content.image = UIImage(systemName: "music.note")
+        }
+        content.imageProperties.cornerRadius = 10
+        content.imageProperties.maximumSize = CGSize(width: 75, height: 575)
+        content.imageProperties.reservedLayoutSize = CGSize(width: 75, height: 75)
+        cell.contentConfiguration = content
+        return cell
+    }
+    
+    
+    @IBAction func exitPressed(_ sender: Any) {
+        self.dismiss(animated: true)
+    }
 }
