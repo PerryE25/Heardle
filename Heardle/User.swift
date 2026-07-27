e4810ac8e344e012383686f25e6524d852733185
PerryE25 <perryehimuh@gmail.com>
2026-07-27 08:18:26 -0500

    edited user class again

--- Heardle/User.swift
+++ Heardle/User.swift
@@ -3,6 +3,24 @@
 //  Heardle
 //
 //  Created by Ehimuh, Perry on 7/27/26.
+//  Project: Heardle
+//  Team Number: 3
+//  Team Members: Jeremiah Franklin, Victor Sanchez, Haroon Memon, Perry Ehimuh
+//  Course: CS371L
 //
 
 import Foundation
+
+// A class to store user's name/pts/image
+class User {
+    
+    var displayName: String
+    var points: Int
+    var displayImage: String?
+    
+    init(displayName: String, points: Int, displayImage: String? = nil) {
+        self.displayName = displayName
+        self.points = points
+        self.displayImage = displayImage
+    }
+}
